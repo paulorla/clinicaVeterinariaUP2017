db033f27ce9f431a20688b3507383ec6eed32c8a
paulorla <paulorla@gmail.com>
2017-05-11 19:38:19 -0300

    Inicio da implementação do EAGER

--- Projeto/clinicavet/src/main/java/com/up/clinicaveterinaria/Main.java
+++ Projeto/clinicavet/src/main/java/com/up/clinicaveterinaria/Main.java
@@ -4,47 +4,20 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import com.up.clinicaveterinaria.dao.AnimalDAO;
 import com.up.clinicaveterinaria.dao.ConsultaDAO;
+import com.up.clinicaveterinaria.model.Animal;
 import com.up.clinicaveterinaria.model.Consulta;
 
 public class Main {
 	public static void main(String[] args) {
 		try{
-			SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+			AnimalDAO animalDAO = new AnimalDAO();
+			Animal animal = animalDAO.buscarEager(4L);
 			
-			ConsultaDAO consultaDAO = new ConsultaDAO();
-			List<Consulta> consultas = consultaDAO.listar();
-			for(Consulta c : consultas){
-				System.out.println(c.getAnimal().getId() +
-						"\t" + dateFormat.format(c.getDataHoraConsulta()) + 
-						"\t" + c.getVeterinario().getId());
-			}
+			System.out.println(animal.getNome() + "\t" + animal.getEspecie().getNome());
 			
-			consultas.get(0).getVeterinario().setId(4L);
-			consultaDAO.atualizar(consultas.get(0));
 			
-			consultas = consultaDAO.listar();
-			for(Consulta c : consultas){
-				System.out.println(c.getAnimal().getId() +
-						"\t" + dateFormat.format(c.getDataHoraConsulta()) + 
-						"\t" + c.getVeterinario().getId());
-			}
-			
-			Consulta resultadoBuscar = consultaDAO.buscar(consultas.get(0).getConsultaId());
-			
-			System.out.println(resultadoBuscar.getAnimal().getId() +
-					"\t" + dateFormat.format(resultadoBuscar.getDataHoraConsulta()) + 
-					"\t" + resultadoBuscar.getVeterinario().getId());
-			
-			Consulta novaConsulta = new Consulta();
-			novaConsulta.setAnimal(resultadoBuscar.getAnimal());
-			novaConsulta.setDataHoraConsulta(new Date());
-			novaConsulta.setVeterinario(resultadoBuscar.getVeterinario());
-			novaConsulta.setResponsavelAgendamento(resultadoBuscar.getResponsavelAgendamento());
-			
-			consultaDAO.persistir(novaConsulta);
-			
-			consultaDAO.remover(novaConsulta.getConsultaId());
 		}catch(Exception e){
 			e.printStackTrace();
 		}
--- Projeto/clinicavet/src/main/java/com/up/clinicaveterinaria/dao/AnimalDAO.java
+++ Projeto/clinicavet/src/main/java/com/up/clinicaveterinaria/dao/AnimalDAO.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import com.up.clinicaveterinaria.jdbc.ConnectionFactory;
 import com.up.clinicaveterinaria.model.Animal;
-import com.up.clinicaveterinaria.model.Dono;
+import com.up.clinicaveterinaria.model.Especie;
 
 public class AnimalDAO implements IGenericDAO<Animal, Long>{
 
@@ -77,12 +77,72 @@ public class AnimalDAO implements IGenericDAO<Animal, Long>{
 			statement = con.prepareStatement(sql);
 			statement.setLong(1, id);
 			resultSet = statement.executeQuery();
-			while(resultSet.next()){
+			if(resultSet.next()){
 				retorno = new Animal();
 				
 				retorno.setId(id);
 				retorno.setNome(resultSet.getString("nome"));
 				retorno.setNascimento(resultSet.getDate("nascimento"));
+			}
+			return retorno;
+		}catch(Exception e){
+			exp = e;
+		}finally{
+			try{
+				if(resultSet != null)
+					resultSet.close();
+			}catch(Exception e){
+				exp = e;
+				//logar exceção
+			}
+			try{
+				if(statement != null)
+					statement.close();
+			}catch(Exception e){
+				exp = e;
+				//logar exceção
+			}
+			try{
+				if(con != null)
+					con.close();
+			}catch(Exception e){
+				exp = e;
+				//logar exceção
+			}
+		}
+		throw exp;//lançando somente a última exceção gerada para simplificar!
+	}
+	
+	public Animal buscarEager(Long id) throws Exception {
+		Animal retorno = null;
+		Connection con=null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		Exception exp = null;
+		try{
+			con = connectionFactory.getConnection();
+			String sql = "SELECT a.nome as nomeanimal, "
+					+ "a.nascimento as nascimentoanimal, "
+					+ "e.id as idespecie, e.nome as nomeespecie, "
+					+ "e.descricao as descricaoespecie "
+					+ "FROM ANIMAL a "
+					+ "INNER JOIN especie e on (a.especie_id = e.id) "
+					+ "where id=?";
+			statement = con.prepareStatement(sql);
+			statement.setLong(1, id);
+			resultSet = statement.executeQuery();
+			if(resultSet.next()){
+				retorno = new Animal();
+				
+				retorno.setId(id);
+				retorno.setNome(resultSet.getString("nomeanimal"));
+				retorno.setNascimento(resultSet.getDate("nascimentoanimal"));
+				
+				Especie e = new Especie();
+				e.setId(resultSet.getLong("idespecie"));
+				e.setNome(resultSet.getString("nomeespecie"));
+				e.setDescricao("descricaoespecie");
+				retorno.setEspecie(e);
 			}
 			return retorno;
 		}catch(Exception e){
