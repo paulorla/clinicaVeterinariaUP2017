42f20ad1ebe479bbcb07c8a627ff26d11f7886ef
paulorla <paulorla@gmail.com>
2017-08-24 20:29:50 -0300

    Primeira aula 2017-08-17

---
+++ Projeto/clinicavet/src/main/java/com/up/clinicaveterinaria/mb/UserMB.java
@@ -0,0 +1,16 @@
+package com.up.clinicaveterinaria.mb;
+
+import com.up.clinicaveterinaria.model.Funcionario;
+
+public class UserMB {
+
+	private Funcionario funcionario;
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+}
---
+++ Projeto/clinicavet/src/main/java/com/up/clinicaveterinaria/util/JSFMessageUtil.java
@@ -0,0 +1,45 @@
+package com.up.clinicaveterinaria.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.context.RequestContext;
+
+
+public class JSFMessageUtil {
+	
+	private static final String CABECALHO_INFO = "Informação";
+	private static final String CABECALHO_AVISO = "Aviso";
+	private static final String CABECALHO_ERRO = "Erro";
+	
+	public static void sendInfoMessageToUser(String message) {
+		JSFMessageUtil.sendInfoMessageToUser(CABECALHO_INFO,message);
+	}
+	
+	public static void sendInfoMessageToUser(String title, String message) {
+		FacesMessage facesMessage = createMessage(FacesMessage.SEVERITY_INFO, title, message);
+		addMessageToJsfContext(facesMessage);
+	}
+	
+	public static void sendWarningMessageToUser(String message) {
+		FacesMessage facesMessage = createMessage(FacesMessage.SEVERITY_WARN, CABECALHO_AVISO , message);
+		addMessageToJsfContext(facesMessage);
+	}
+
+	public static void sendErrorMessageToUser(String message) {
+		FacesMessage facesMessage = createMessage(FacesMessage.SEVERITY_ERROR, CABECALHO_ERRO , message);
+		RequestContext context = RequestContext.getCurrentInstance();
+		context.addCallbackParam("erroRequest", true);
+		
+		addMessageToJsfContext(facesMessage);
+	}
+
+	private static FacesMessage createMessage(Severity severity, String sumario, String mensagemErro) {
+		return new FacesMessage(severity, sumario, mensagemErro);
+	}
+
+	private static void addMessageToJsfContext(FacesMessage facesMessage) {
+		FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+	}
+}
