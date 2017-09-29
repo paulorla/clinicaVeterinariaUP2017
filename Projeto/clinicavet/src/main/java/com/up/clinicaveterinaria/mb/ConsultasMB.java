b1d47c200762c85dc0d33829b4938fa922574ae2
paulorla <paulorla@gmail.com>
2017-09-28 21:50:45 -0300

    Aula 2017-09-28

--- Projeto/clinicavet/src/main/java/com/up/clinicaveterinaria/mb/ConsultasMB.java
+++ Projeto/clinicavet/src/main/java/com/up/clinicaveterinaria/mb/ConsultasMB.java
@@ -1,17 +1,21 @@
 package com.up.clinicaveterinaria.mb;
 
+import java.util.Date;
 import java.util.List;
 
 import com.up.clinicaveterinaria.facade.FuncionarioFacade;
 import com.up.clinicaveterinaria.model.Funcionario;
+import com.up.clinicaveterinaria.util.DataHoraUtil;
 
 public class ConsultasMB {
 	
 	private List<Funcionario> veterinarios;
 	private Funcionario veterinarioSelecionado;
+	private Date diaConsultas;
 	
 	public ConsultasMB() {
 		try {
+			diaConsultas = DataHoraUtil.getDiaAtual();
 			veterinarioSelecionado = null;
 			veterinarios = (new FuncionarioFacade()).listarVeterinarios();
 		}catch(Exception e) {
@@ -29,5 +33,14 @@ public class ConsultasMB {
 
 	public List<Funcionario> getVeterinarios() {
 		return veterinarios;
+	}
+
+	public Date getDiaConsultas() {
+		return diaConsultas;
+	}
+
+	public void setDiaConsultas(Date diaConsultas) {
+		this.diaConsultas = diaConsultas;
+		System.out.println(diaConsultas);
 	}
 }
